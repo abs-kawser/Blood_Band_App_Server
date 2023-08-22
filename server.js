03d46a5836073339563c964fa428f066666cdeb0
Abs kawser <abskawser77000@gmail.com>
2023-08-22 20:55:34 +0600

    server done

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const morgan = require("morgan");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+const router = express.Router();
+//dot config
+dotenv.config();
+
+//mongodb connection
+connectDB();
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+//routes
+// 1 test route
+
+
+// Home page route.
+
+app.use("/api/v1/test", require("./routes/testRoutes"));
+app.use("/api/v1/auth", require("./routes/authRoutes"));
+app.use("/api/v1/inventory", require("./routes/inventoryRoutes"));
+app.use("/api/v1/analytics", require("./routes/analyticsRoutes"));
+app.use("/api/v1/admin", require("./routes/adminRoutes"));
+
+app.use("/", function (req, res) {
+  res.send("Wellcome to home page");
+});
+
+//port
+const PORT = process.env.PORT || 8080;
+
+//listen
+app.listen(PORT, () => {
+  console.log(
+    `Node Server Running In ${process.env.DEV_MODE} ModeOn Port ${process.env.PORT}`
+      .bgBlue.white
+  );
+});
